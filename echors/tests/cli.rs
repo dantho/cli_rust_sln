d1fb9bddb5cbc4afcae683b315f648a592131cc2
Dan Thornton <dan.thornton+Git@thornton.me>
2023-03-25 14:21:05 -0400

    end of Chapter 2

--- echors/tests/cli.rs
+++ echors/tests/cli.rs
@@ -1,33 +1,63 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echors").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echors")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
     // Try again with flag, but still no args
-    let mut cmd = Command::cargo_bin("echors").unwrap();
+    let mut cmd = Command::cargo_bin("echors")?;
     cmd
         .arg("-n")
         .assert()
             .failure()
             .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echors").unwrap();
+fn dies_too_many_flags() -> TestResult {
+    let mut cmd = Command::cargo_bin("echors")?;
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    // Try again with flag, but still no args
+    let mut cmd = Command::cargo_bin("echors")?;
+    cmd
+        .arg("-n")
+        .arg("-n")
+        .assert()
+            .failure()
+            .stderr(predicate::str::contains("USAGE"));
+    Ok(())
+}
+
+#[test]
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echors")?;
     cmd.arg("Hello").assert().success();
-    let mut cmd = Command::cargo_bin("echors").unwrap();
+    let mut cmd = Command::cargo_bin("echors")?;
     cmd
         .arg("Hello")
         .arg("world")
         .assert()
             .stdout(predicate::str::contains("Hello"))
-            .stdout(predicate::str::contains("world\n"));
-    let mut cmd = Command::cargo_bin("echors").unwrap();
+            .stdout(predicate::str::contains("world\r\n"));
+        let mut cmd = Command::cargo_bin("echors")?;
+        cmd
+            .arg("-n")
+            .arg("Hello")
+            .arg("world")
+            .assert()
+                .stdout(predicate::str::contains("Hello"))
+                .stdout(predicate::str::contains("world"))
+                .stdout(predicate::str::contains("\n").not());
+    let mut cmd = Command::cargo_bin("echors")?;
     cmd
         .arg("-n")
         .arg("Hello")
@@ -36,4 +66,44 @@ fn runs() {
             .stdout(predicate::str::contains("Hello"))
             .stdout(predicate::str::contains("world"))
             .stdout(predicate::str::contains("\n").not());
+    let mut cmd = Command::cargo_bin("echors")?;
+    cmd
+        .arg("--omit_newline")
+        .arg("Hello")
+        .arg("world")
+        .assert()
+            .stdout(predicate::str::contains("Hello"))
+            .stdout(predicate::str::contains("world"))
+            .stdout(predicate::str::contains("\n").not());
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&vec!["Hello there"], "tests\\expected\\hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&vec!["Hello", "there"], "tests\\expected\\hello2.txt")
+}
+
+#[test]
+fn hello1_omit_newline() -> TestResult {
+    run(&vec!["-n", "Hello there"], "tests\\expected\\hello1.n.txt")
+}
+
+#[test]
+fn hello2_omit_newline() -> TestResult {
+    run(&vec!["Hello", "-n", "there"], "tests\\expected\\hello2.n.txt")
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echors")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
 }

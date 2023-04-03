0c7a22b6269e9001d951ad9ecaf42adbc411814a
Dan Thornton <dan.thornton+Git@thornton.me>
2023-04-03 19:31:06 -0400

    headrs config functional

---
+++ README.md
@@ -0,0 +1,4 @@
+Working through the examples from the (new arrival) library book, _Command Line Rust -- A Project Based Primer for Writing Rust CLIs_ by *Ken Youens-Clarke*.
+
+The publisher's link is here: [Command-Line Rust](https://www.oreilly.com/library/view/command-line-rust/9781098109424/)  
+The book's source code repo is on GitHub: [kyclark/command-line-rust](https://github.com/kyclark/command-line-rust)  
---
+++ headrs/src/lib.rs
@@ -0,0 +1,104 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>,
+}
+
+type HeadResult<T> = Result<T, Box<dyn Error>>;
+
+pub fn run(config: Config) -> HeadResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> HeadResult<usize> {
+    match val.parse() {
+        Ok(num) => Ok(num),
+        Err(_) => Err(val.into())
+    }
+}
+
+pub fn get_args() -> HeadResult<Config> {
+    let matches = App::new("Headrs")
+        .version("0.1.0")
+        .author("Dan Thornton <dan.thornton@thornton.me")
+        .about("Head in Rust")
+        .arg(
+            Arg::with_name("file_or_stdin")
+                .value_name("File")
+                .help("Input filename or '-' for STDIN")
+                .multiple(true)
+                .default_value("-")
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .help("Number of lines")
+                .case_insensitive(true)
+                .default_value("10")
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Number of bytes")
+                .takes_value(true)
+                .required(false)
+                .case_insensitive(true)
+                .conflicts_with("lines")
+        )
+        .get_matches();
+
+        let files = matches.values_of_lossy("file_or_stdin").unwrap(); // Has a default
+        let lines = match parse_positive_int(matches.value_of("lines").unwrap()) {
+            Ok(num) => num,
+            Err(e) => return Err(format!("illegal line count -- {}" , e.to_string()).into()),
+        };
+        let bytes = if matches.is_present("bytes") {
+            match parse_positive_int(matches.value_of("bytes").unwrap()) {
+                Ok(num) => Some(num),
+                Err(e) => return Err(format!("illegal byte count -- {}" , e.to_string()).into()),
+            }
+        } else {
+            None
+        };
+        Ok(Config {
+            files,
+            lines,
+            bytes,
+        })
+
+    }
+
+    pub fn open(filename: &str) -> HeadResult<Box<dyn BufRead>> {
+        match filename {
+            "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+            _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+        }
+    }
+
+
+    #[test]
+    fn test_parse_positive_int() {
+        // 3 is an OK positive integer
+        let res = parse_positive_int("3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), 3);
+
+        // -3 is a BAD positive integer
+        let res = parse_positive_int("-3");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "-3".to_string());
+
+        // non-numeric string is an error
+        let res = parse_positive_int("foo");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+    }
